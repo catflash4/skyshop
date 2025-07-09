aa0755eb18e83da988ee7ffd243a1483573153c3
MMPC <mrcatflashyt@mail.ru>
2025-07-09 13:46:14 +0300

    Search

--- src/main/java/org/skypro/skyshop/service/SearchService.java
+++ src/main/java/org/skypro/skyshop/service/SearchService.java
@@ -23,3 +23,6 @@ public class SearchService {
                 .collect(Collectors.toCollection(ArrayList::new));
     }
 }
+
+
+
--- src/main/java/org/skypro/skyshop/service/StorageService.java
+++ src/main/java/org/skypro/skyshop/service/StorageService.java
@@ -64,3 +64,6 @@ public class StorageService {
         return result;
     }
 }
+
+
+
