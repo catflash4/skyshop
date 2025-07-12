aa30fb70e92e5d2d7fabcd79353edff89b9695cc
MMPC <mrcatflashyt@mail.ru>
2025-07-12 23:38:31 +0300

    Search

---
+++ src/main/java/org/skypro/skyshop/exceptions/IllegalDiscountException.java
@@ -0,0 +1,8 @@
+package org.skypro.skyshop.exceptions;
+
+
+public class IllegalDiscountException extends IllegalArgumentException{
+    public IllegalDiscountException() {
+        super();
+    }
+}
